332dd7e090aff043d0aac89420e15585e89915be
Anchal Srivastava <srivastavaanchal000@gmail.com>
2021-02-14 10:47:28 +0530

    CourseraReact(week2): Functional component

--- CourseraReactFrontEnd/confusion/src/components/DishtdetailComponent.js
+++ CourseraReactFrontEnd/confusion/src/components/DishtdetailComponent.js
@@ -1,46 +1,51 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
 
-    renderComments(commentArray) {
-        const menu = commentArray.map((com) => {
-            if (com == null) return (<div></div>);
-            return (
-                <div key={com.id} className="ul list-unstyled">
-                    <p >{com.comment}</p>
-                    <p > -- {com.author},  {new Intl.DateTimeFormat('en-US',{year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(com.date)))}</p>
-                </div>
-            );
-        });
-        return menu;
-    }
+function RenderComments({ commentArray }) {
+    const menu = commentArray.map((com) => {
+        if (com == null) return (<div></div>);
+        return (
+            <div key={com.id} className="ul list-unstyled">
+                <p >{com.comment}</p>
+                <p > -- {com.author},  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(com.date)))}</p>
+            </div>
+        );
+    });
+    return menu;
+}
 
-    render() {
-        if (this.props.dish) {
-            return (
-                <div className="container">
+function RenderDish({dish}) {
+    console.log(dish.name);
+    return (
+        <Card>
+            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle heading>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+const DishDetail = (props) =>{
+    if (props.dish) {
+        return (
+            <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" object src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                                <CardTitle heading>{this.props.dish.name}</CardTitle>
-                                <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                        <RenderDish dish={props.dish}/>
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>               
-                        {this.renderComments(this.props.dish.comments)}
+                        <h4>Comments</h4>
+                        <RenderComments commentArray = {props.dish.comments}/>
                     </div>
                 </div>
-                </div>
-            );
-        }
-        else {
-            return (<div></div>);
-        }
+            </div>
+        );
+    }
+    else {
+        return (<div></div>);
     }
 }
 
